33ffa33904ce66f6e2ae072bc50eefb197d9f55a
Nitish kumar <nitishkumarnk9345543@gmail.com>
2025-09-20 17:36:18 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cors = require('cors'); 
+
+const app = express();
+const port = 3000;
+
+
+app.use(cors());
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'geolocdb'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('MySQL connection error:', err);
+    return;
+  }
+  console.log(' Connected to MySQL');
+});
+
+app.post('/submit', (req, res) => {
+  const { name, email, message } = req.body;
+  console.log(' Form Data Received:', { name, email, message });
+
+  const query = 'INSERT INTO contact_messages (name, email, message) VALUES (?, ?, ?)';
+  db.execute(query, [name, email, message], (err, results) => {
+    if (err) {
+      console.error(' Error inserting data:', err);
+      return res.status(500).json({ success: false });
+    }
+    res.json({ success: true });
+  });
+});
+app.post('/save-places', (req, res) => {
+  const places = req.body;
+
+  if (!Array.isArray(places)) {
+    return res.status(400).json({ error: 'Invalid data format' });
+  }
+
+  const query = `
+    INSERT INTO places (name, category, address, lat, lon)
+    VALUES ?
+    ON DUPLICATE KEY UPDATE
+      name = VALUES(name)  
+  `;
+
+  const values = places.map(place => [
+    place.name || '',
+    place.category || '',
+    place.address || '',
+    place.lat || 0,
+    place.lon || 0
+  ]);
+
+  db.query(query, [values], (err, result) => {
+    if (err) {
+      console.error('Error inserting places:', err);
+      return res.status(500).json({ success: false, error: err });
+    }
+    res.json({ success: true, inserted: result.affectedRows });
+  });
+});
+
+app.listen(port, () => {
+  console.log(` Server running at http://localhost:${port}`);
+});
